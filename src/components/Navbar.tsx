38e1793c8c61395ca500597aba03b1a60900c1ba
tommyliu-95518 <Tommy.Liu95518@outlook.com>
2025-12-08 10:04:23 +0900

    certification changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 		{ path: '/experience', label: 'Experience' },
 		{ path: '/skills', label: 'Skills' },
 		{ path: '/projects', label: 'Projects' },
-		{ path: '/certificates', label: 'Certificates' },
+		// { path: '/certificates', label: 'Certificates' },
 		{ path: '/contact', label: 'Contact' }
 	];
 
